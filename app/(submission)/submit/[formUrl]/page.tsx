665559e964565072863f24dea93c49efa45996eb
dj1samsoe <dhanyh86@gmail.com>
2024-02-06 00:29:33 +0700

    feat: metadata for form submission

--- app/(submission)/submit/[formUrl]/page.tsx
+++ app/(submission)/submit/[formUrl]/page.tsx
@@ -1,6 +1,27 @@
 import { FormElementInstance } from "@/app/(dashboard)/_components/FormElements";
 import { GetFormContentByUrl } from "@/app/actions/form";
 import FormSubmitComponent from "../../_components/FormSubmitComponent";
+import { METADATA } from "@/lib/metadata";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { formUrl: string };
+}) {
+  const form = await GetFormContentByUrl(params.formUrl);
+  if (!form) {
+    return {
+      title: "Form not found",
+    };
+  }
+  return {
+    title: `Form Submission ${METADATA.exTitle}`,
+    description: "Form Submission in Dhanhid Form - Simple Form Builder",
+    alternates: {
+      canonical: `${process.env.DOMAIN}/submit/${params.formUrl}`,
+    },
+  };
+}
 
 export default async function SubmitPage({
   params,
